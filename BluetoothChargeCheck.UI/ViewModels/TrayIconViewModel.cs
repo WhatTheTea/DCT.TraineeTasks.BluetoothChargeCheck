922d1a489be0d1c2722c0e17797632dd915eafd7
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-24 15:52:00 +0300

    Add tray icon VM

---
+++ BluetoothChargeCheck.UI/ViewModels/TrayIconViewModel.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file = "TrayIconViewModel.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.ViewModels;
+
+public partial class TrayIconViewModel : ObservableObject
+{
+    [RelayCommand]
+    private void ExitApplication() =>
+        Application.Current.Shutdown();
+}
