d071d370567775f086a0eb537e34b58d337d0be4
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-25 16:20:52 +0300

    Add interfaces

---
+++ BluetoothChargeCheck.UI/Models/IBluetoothDevice.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file = "IBluetoothDevice.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System.ComponentModel;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
+
+public interface IBluetoothDevice : INotifyPropertyChanged
+{
+    string Name { get; }
+    double Charge { get; }
+    bool Connected { get; }
+}
---
+++ BluetoothChargeCheck.UI/Services/IBluetoothService.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file = "IBluetoothService.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Services;
+
+public interface IBluetoothService : INotifyPropertyChanged
+{
+    ReadOnlyObservableCollection<IBluetoothDevice> Devices { get; }
+    ReadOnlyObservableCollection<IBluetoothDevice> Connected { get; }
+}
