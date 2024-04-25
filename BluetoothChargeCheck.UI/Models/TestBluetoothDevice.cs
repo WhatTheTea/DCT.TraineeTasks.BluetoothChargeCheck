f321ae84a1dad6c9ad0bad750365daad32925d5d
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-25 16:34:07 +0300

    Add test implementations

---
+++ BluetoothChargeCheck.UI/Models/TestBluetoothDevice.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file = "TestBluetoothDevice.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
+
+internal partial class TestBluetoothDevice : ObservableObject, IBluetoothDevice
+{
+    [ObservableProperty] private string name = string.Empty;
+    [ObservableProperty] private double charge;
+    [ObservableProperty] private bool connected;
+
+    public async Task ChargeCyclingAsync(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            if (this.Charge <= 100)
+            {
+                this.Charge += 10;
+            }
+            else
+            {
+                this.Charge = 0;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(2), token);
+        }
+    }
+}
---
+++ BluetoothChargeCheck.UI/Services/TestBluetoothService.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file = "TestBluetoothService.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Services;
+
+public class TestBluetoothService : ObservableObject, IBluetoothService
+{
+    public ReadOnlyObservableCollection<IBluetoothDevice> Devices { get; private set; }
+    public ReadOnlyObservableCollection<IBluetoothDevice> Connected { get; private set; }
+}
