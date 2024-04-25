30be357d9ac0c49d26848fb2d3b56a2d70ab3801
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-25 21:54:14 +0300

    Change Hardcodet taskbar icon -> H.NotifyIcon

--- BluetoothChargeCheck.UI/App.xaml.cs
+++ BluetoothChargeCheck.UI/App.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Drawing;
 using System.Windows;
 using System.Windows.Media;
-using Hardcodet.Wpf.TaskbarNotification;
+
 
 namespace DCT.TraineeTasks.BluetoothChargeCheck.UI;
 
@@ -10,18 +10,6 @@ namespace DCT.TraineeTasks.BluetoothChargeCheck.UI;
 /// </summary>
 public partial class App : Application
 {
-    private TaskbarIcon trayIcon;
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-        this.trayIcon = this.FindResource("TrayIconResource") as TaskbarIcon
-            ?? throw new InvalidOperationException("Can't find resource");
-    }
-
-    protected override void OnExit(ExitEventArgs e)
-    {
-        this.trayIcon.Dispose();
-        base.OnExit(e);
-    }
+    
 }
 
