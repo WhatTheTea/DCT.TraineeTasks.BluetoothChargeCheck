6e9e2b63ce059e68150df36aac8c23686ef7a8f1
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-25 21:53:29 +0300

    Add main window

---
+++ BluetoothChargeCheck.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file = "MainWindowViewModel.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DCT.TraineeTasks.BluetoothChargeCheck.UI.Services;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    public IBluetoothService BluetoothService { get; init; } = new TestBluetoothService();
+
+    [RelayCommand]
+    private void AddTaskbarIcon()
+    {
+
+    }
+
+}
---
+++ BluetoothChargeCheck.UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file = "MainWindow.xaml.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Views;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
