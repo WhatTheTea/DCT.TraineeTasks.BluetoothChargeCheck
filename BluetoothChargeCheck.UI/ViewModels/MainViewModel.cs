d57c560b9c73cf2f8aa17e25617cc89c3ff58071
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-26 01:51:44 +0300

    Add dynamic font icons

--- BluetoothChargeCheck.UI/ViewModels/MainViewModel.cs
+++ BluetoothChargeCheck.UI/ViewModels/MainViewModel.cs
@@ -2,20 +2,25 @@
 // Copyright (c) Digital Cloud Technologies.All rights reserved.
 // </copyright>
 
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DCT.TraineeTasks.BluetoothChargeCheck.UI.Services;
+using H.NotifyIcon;
 
 namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
     public IBluetoothService BluetoothService { get; init; } = new TestBluetoothService();
+    private List<TaskbarIcon> iconInstances = [];
 
     [RelayCommand]
     private void AddTaskbarIcon()
     {
-
+        var app = Application.Current;
+        var taskbarIcon = (TaskbarIcon)app.FindResource("TrayIconResource")!;
+        taskbarIcon.ForceCreate(false);
+        this.iconInstances.Add(taskbarIcon);
     }
-
 }
--- BluetoothChargeCheck.UI/ViewModels/TrayIconViewModel.cs
+++ BluetoothChargeCheck.UI/ViewModels/TrayIconViewModel.cs
@@ -4,25 +4,36 @@
 
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Converters;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
+using H.NotifyIcon;
 using Wpf.Ui.Appearance;
+using Wpf.Ui.Markup;
 
 namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.ViewModels;
 
 public partial class TrayIconViewModel : ObservableObject
 {
+    private string[] chargeStrings = [
+        "\uEBA0", "\uEBA1","\uEBA2","\uEBA3","\uEBA4","\uEBA5",
+        "\uEBA6","\uEBA7","\uEBA8","\uEBA9","\uEBAA"];
+
     [RelayCommand]
     private void ExitApplication() =>
         Application.Current.Shutdown();
-
-    public ImageSource IconSource => this.ChargeToIcon(this.BluetoothDevice.Charge);
+    
     public IBluetoothDevice BluetoothDevice;
+
+    [ObservableProperty] private Brush? accent = new BrushConverter().ConvertFrom("White") as Brush;
+    [ObservableProperty] private string glyph;
+
     public TrayIconViewModel()
     {
+        this.Glyph = this.chargeStrings[0];
         var testDevice = new TestBluetoothDevice();
         this.BluetoothDevice = testDevice;
         Task.Run(() => testDevice.ChargeCyclingAsync(CancellationToken.None));
@@ -30,22 +41,8 @@ public partial class TrayIconViewModel : ObservableObject
         {
             if (args.PropertyName == nameof(this.BluetoothDevice.Charge))
             {
-                this.OnPropertyChanged(nameof(this.IconSource));
+                this.Glyph = this.chargeStrings[(int)(this.BluetoothDevice.Charge / 10)];
             }
         };
     }
-
-    private ImageSource FindIcon(int iconNumber)
-    {
-        if (iconNumber <= 10 && iconNumber >= 0)
-        {
-            var appTheme = Enum.GetName(ApplicationThemeManager.GetAppTheme());
-            return new BitmapImage(new Uri(
-                $"pack://application:,,,/BluetoothChargeCheck.UI;component/Resources/Images/Battery-{appTheme}/Battery{iconNumber}.ico"));
-        }
-
-        throw new ArgumentOutOfRangeException(nameof(iconNumber));
-    }
-
-    private ImageSource ChargeToIcon(double charge) => this.FindIcon((int)(charge / 10));
 }
