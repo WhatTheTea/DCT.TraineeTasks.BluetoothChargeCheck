21bc1b8668a68b8f104ec0db6ecd95fdd5165fd8
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-04-29 23:52:45 +0300

    Add theme watcher

--- BluetoothChargeCheck.UI/Views/MainWindow.xaml.cs
+++ BluetoothChargeCheck.UI/Views/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Digital Cloud Technologies.All rights reserved.
 // </copyright>
 
+using Wpf.Ui.Controls;
+
 namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Views;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -11,6 +13,9 @@ public partial class MainWindow
     public MainWindow()
     {
         InitializeComponent();
+
+        this.Loaded += (_, _) => Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this,
+                                                                            WindowBackdropType.Auto);
     }
 
 
