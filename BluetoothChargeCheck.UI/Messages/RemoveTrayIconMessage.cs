7c968e43913520516ec753479e4095c53c95a55d
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-04-30 00:52:46 +0300

    Add remove tray icon message

---
+++ BluetoothChargeCheck.UI/Messages/RemoveTrayIconMessage.cs
@@ -0,0 +1,9 @@
+﻿// <copyright file = "RemoveTrayIconMessage.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using DCT.TraineeTasks.BluetoothChargeCheck.UI.ViewModels;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Messages;
+internal class RemoveTrayIconMessage(TrayIconViewModel value) : ValueChangedMessage<TrayIconViewModel>(value);
