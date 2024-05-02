fba31d742ba4f000a51f4085bd767461ae69eee9
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-02 14:06:42 +0300

    Remove window manipulation commands

--- BluetoothChargeCheck.UI/ViewModels/MainViewModel.cs
+++ BluetoothChargeCheck.UI/ViewModels/MainViewModel.cs
@@ -2,19 +2,14 @@
 // Copyright (c) Digital Cloud Technologies.All rights reserved.
 // </copyright>
 
-using System.CodeDom.Compiler;
 using System.Collections.Immutable;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using CommunityToolkit.Mvvm.Messaging;
-using DCT.TraineeTasks.BluetoothChargeCheck.UI.Messages;
-using DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
 using DCT.TraineeTasks.BluetoothChargeCheck.UI.Services;
 using DCT.TraineeTasks.BluetoothChargeCheck.UI.ViewModels.Device;
 using DCT.TraineeTasks.BluetoothChargeCheck.UI.Views;
+using DCT.TraineeTasks.BluetoothChargeCheck.UI.Views.AppTrayIcon;
 using H.NotifyIcon;
 
 namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.ViewModels;
@@ -40,34 +35,5 @@ public partial class MainViewModel : ObservableObject
                 this.TrayIconViewModels = newItems.Cast<DeviceViewModel>().ToImmutableArray();
             }
         };
-    }
-
-    // Main tray icon
-
-    [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(this.HideCommand))]
-    private bool canExecuteHideCommand = true;
-
-    [RelayCommand(CanExecute = nameof(this.CanExecuteHideCommand))]
-    private void Hide()
-    {
-        Application.Current.MainWindow?.Hide(true);
-        this.CanExecuteHideCommand = false;
-        this.CanExecuteShowCommand = true;
-    }
-    [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(this.ShowCommand))]
-    private bool canExecuteShowCommand = true;
-
-    [RelayCommand(CanExecute = nameof(this.CanExecuteShowCommand))]
-    private void Show()
-    {
-        Application.Current.MainWindow = new MainWindow
-        {
-            DataContext = this
-        };
-        Application.Current.MainWindow?.Show(true);
-        this.CanExecuteHideCommand = true;
-        this.CanExecuteShowCommand = false;
     }
 }
