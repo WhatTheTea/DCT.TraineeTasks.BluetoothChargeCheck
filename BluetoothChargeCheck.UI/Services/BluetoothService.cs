f7d0d6a1fddc7a183d4cfc636f845fce9c46ba1a
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-13 23:25:00 +0300

    Add 32feet for BluetoothLE support

---
+++ BluetoothChargeCheck.UI/Services/BluetoothService.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file = "BluetoothService.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
+using InTheHand.Bluetooth;
+using BluetoothDevice = DCT.TraineeTasks.BluetoothChargeCheck.UI.Models.BluetoothDevice;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Services;
+
+public partial class BluetoothService : ObservableObject, IBluetoothService
+{
+    [ObservableProperty]
+    private ObservableCollection<IBluetoothDevice> devices = [];
+    [ObservableProperty]
+    private ObservableCollection<IBluetoothDevice> connected = [];
+
+    public BluetoothService()
+    {
+        this.ScanDevices();
+    }
+
+    private void ScanDevices() => App.Current.Dispatcher.BeginInvoke(async () =>
+    {
+        var devices = await Bluetooth.GetPairedDevicesAsync();
+        this.Devices = new ObservableCollection<IBluetoothDevice>(devices.Select(x =>
+            new BluetoothDevice(x.Name, 0, x.IsPaired)));
+        this.Connected = this.Devices;
+    });
+}
