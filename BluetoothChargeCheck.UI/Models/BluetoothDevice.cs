67ed1df36298d52f941a9e25d8ef9b626613e43e
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-13 23:24:09 +0300

    Add basic bluetooth device implementation

---
+++ BluetoothChargeCheck.UI/Models/BluetoothDevice.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file = "BluetoothDevice.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Models;
+
+public partial class BluetoothDevice : ObservableObject, IBluetoothDevice
+{
+    [ObservableProperty]
+    private string name;
+    [ObservableProperty]
+    private double charge;
+    [ObservableProperty]
+    private bool connected;
+
+    public BluetoothDevice(string name, double charge, bool connected)
+    {
+        this.name = name;
+        this.charge = charge;
+        this.connected = connected;
+    }
+}
