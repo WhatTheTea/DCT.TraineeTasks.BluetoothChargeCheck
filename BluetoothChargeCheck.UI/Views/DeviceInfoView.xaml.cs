094fc78a9e0c2c7ca26cc2b2799372a442721254
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-17 22:49:05 +0300

    Extract device info to separate control

---
+++ BluetoothChargeCheck.UI/Views/DeviceInfoView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DCT.TraineeTasks.BluetoothChargeCheck.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for DeviceInfoView.xaml
+    /// </summary>
+    public partial class DeviceInfoView : UserControl
+    {
+        public DeviceInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
