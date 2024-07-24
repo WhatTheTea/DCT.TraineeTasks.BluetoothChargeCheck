08a155ee39c18b97da50dc7309e9df6ee6ce5228
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-24 12:36:18 +0300

    Fix App namespace

--- BluetoothChargeCheck.UI/App.xaml.cs
+++ BluetoothChargeCheck.UI/App.xaml.cs
@@ -4,12 +4,14 @@
 
 using System.Windows;
 
+using DCT.BluetoothChargeCheck.ViewModels;
+
 using H.NotifyIcon;
 
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 
-namespace DCT.TraineeTasks.BluetoothChargeCheck.UI;
+namespace DCT.BluetoothChargeCheck;
 
 /// <summary>
 ///     Interaction logic for App.xaml
@@ -22,26 +24,9 @@ public partial class App : Application
     {
         base.OnStartup(e);
 
-        //TryAddIconsFont();
-
         SystemThemeWatcher.Watch(null, WindowBackdropType.Auto);
         this.CreateAppTrayIcon();
     }
-
-    /// H.NotifyIcons tries to convert <see cref="System.Windows.Media.FontFamily"/> to <see cref="System.Drawing.FontFamily"/>
-    /// by searching for fonts in system. Thus I forced to install font for Win10.
-    //private static void TryAddIconsFont()
-    //{
-    //    if (Environment.OSVersion.Platform == PlatformID.Win32NT
-    //        && Environment.OSVersion.Version.Major < 11)
-    //    {
-    //        int result = FontManager.AddFontResource("Fonts\\SegoeFluentIcons.ttf");
-    //        if (result == 0)
-    //        {
-    //            throw new InvalidOperationException("Failed to install Segoe Fluent Icons");
-    //        }
-    //    }
-    //}
 
     private void CreateAppTrayIcon()
     {
