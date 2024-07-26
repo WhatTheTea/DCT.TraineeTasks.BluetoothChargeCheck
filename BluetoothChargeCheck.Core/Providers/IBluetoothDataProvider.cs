2f997308ab25d0ac91297a9ce0275cac4f6585be
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-26 13:36:23 +0300

    Add BluetoothDataProvider interface

---
+++ BluetoothChargeCheck.Core/Providers/IBluetoothDataProvider.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file = "IBluetoothDataProvider.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using DCT.BluetoothChargeCheck.Models;
+
+namespace DCT.BluetoothChargeCheck.Core.Providers;
+public interface IBluetoothDataProvider
+{
+    IAsyncEnumerable<BluetoothDeviceData> FetchDevicesAsync();
+    IEnumerable<BluetoothDeviceData> FetchDevices();
+}
