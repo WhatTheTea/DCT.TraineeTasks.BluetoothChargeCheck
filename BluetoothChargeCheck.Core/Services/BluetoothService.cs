9c37728bf09040c212a028a1cc0b7608ab3b08e5
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-29 23:35:40 +0300

    Add bluetooth availability check

--- BluetoothChargeCheck.Core/Services/BluetoothService.cs
+++ BluetoothChargeCheck.Core/Services/BluetoothService.cs
@@ -35,14 +35,15 @@ public class BluetoothService(IBluetoothDataProvider bluetoothProvider)
 
     /// <summary>
     /// Returns collections of device data on specified interval.<br/>
-    /// Runs indefinitely until stopped, can be supplied with scheduler.
+    /// Runs indefinitely until stopped, can be supplied with scheduler. <br/>
     /// </summary>
     public IObservable<IEnumerable<BluetoothDeviceData>> GetDevicesObservable(TimeSpan updateInterval, IScheduler? scheduler = null) =>
         Observable.Interval(updateInterval, scheduler ?? Scheduler.Default)
-            // Prepend tick to fire immediately
-            .Prepend(updateInterval.Ticks)
-            // On each tick get bluetooth data, validate and gather it into array
-            .Select(_ => this.dataProvider.FetchDevicesAsync()
+            .Prepend(updateInterval.Ticks)                            // Prepend tick to fire immediately
+            .Select(x => CheckBluetoothAvailability().ToObservable()) // On each tick check bluetooth availability
+            .Concat()
+            .Where(isAvailable => isAvailable)                        // Continue only when bluetooth is available
+            .Select(_ => this.dataProvider.FetchDevicesAsync()        // then get bluetooth data, validate and gather it into array
                 .ToObservable()
                 .Where(x => this.bluetoothValidator.Validate(x).IsValid)
                 .Aggregate(Array.Empty<BluetoothDeviceData>() as IEnumerable<BluetoothDeviceData>,
