8bb0a3c13cd1c02f5c868818b930c45be1d1473e
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-07-29 21:57:28 +0300

    Add bluetooth availability check method

--- BluetoothChargeCheck.Core/Services/BluetoothService.cs
+++ BluetoothChargeCheck.Core/Services/BluetoothService.cs
@@ -49,4 +49,11 @@ public class BluetoothService(IBluetoothDataProvider bluetoothProvider)
                 (data, device) => data.Append(device)))
             .Concat()
             .Publish().RefCount();
+
+    public static async Task<bool> CheckBluetoothAvailability()
+    {
+        var radios = await Windows.Devices.Radios.Radio.GetRadiosAsync();
+        var bluetoothAdapter = radios.FirstOrDefault(x => x.Kind == Windows.Devices.Radios.RadioKind.Bluetooth);
+        return bluetoothAdapter is not null && bluetoothAdapter.State == Windows.Devices.Radios.RadioState.On;
+    }
 }
